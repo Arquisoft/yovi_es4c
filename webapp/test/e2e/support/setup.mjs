67d7a628c88132bed928c21fc5ab58c665a90152
Pablo Garcia <UO299834@uniovi.es>
2026-04-06 20:57:49 +0200

    Enable headless mode for browser in setup
    Changed headless mode to true for browser launch.

--- webapp/test/e2e/support/setup.mjs
+++ webapp/test/e2e/support/setup.mjs
@@ -12,7 +12,7 @@ setWorldConstructor(CustomWorld)
 
 Before(async function () {
   // Allow turning off headless mode and enabling slow motion/devtools via env vars
-  const headless = false
+  const headless = true
   const slowMo = 0
   const devtools = false
 
